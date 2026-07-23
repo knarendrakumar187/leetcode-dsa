987d65ba4702b84a4d8e9d78e106350989f2144a
knarendrakumar187 <knarendrakumar187@gmail.com>
2026-07-23 15:46:53 +0530

    Time: 2 ms (33.83%) | Memory: 22.6 MB (11.33%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -1,26 +1,11 @@
 class Solution:
     def kthSmallest(self, root, k):
-
-        stack = []          # Stack to store nodes
-        cur = root          # Start from the root
-        count = 0           # Counts visited nodes
-
-        # Continue until there are no nodes left
-        while cur or stack:
-
-            # Go to the leftmost node
-            while cur:
-                stack.append(cur)   # Save current node
-                cur = cur.left      # Move left
-
-            # Leftmost node reached
-            cur = stack.pop()       # Visit the top node
-
-            count += 1              # Increase visited node count
-
-            # If this is the kth visited node
-            if count == k:
-                return cur.val
-
-            # Now visit the right subtree
-            cur = cur.right
+        result=[]
+        def dfs(root):
+            if not root:
+                return None
+            dfs(root.left)
+            result.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        return result[k-1]
