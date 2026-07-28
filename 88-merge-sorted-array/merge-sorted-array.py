abf9dbe1b403b85c02ef59df036c9bdb4f07aef9
knarendrakumar187 <knarendrakumar187@gmail.com>
2026-07-28 07:44:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (76.64%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -15,3 +15,95 @@ class Solution:
             nums1[last] = nums2[n - 1]
             n -= 1
             last -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+            # LeetCode 88 - Merge Sorted Array
+#
+# nums1 = [1,2,3,0,0,0]
+# nums2 = [2,5,6]
+#
+# Initial
+#
+# nums1: [1][2][3][0][0][0]
+#              ↑        ↑
+#              m-1      last
+#
+# nums2: [2][5][6]
+#              ↑
+#              n-1
+#
+# ------------------------------------------------
+#
+# Step 1
+# Compare: 3 vs 6
+#
+# nums1: [1][2][3][0][0][6]
+#              ↑     ↑
+#              m-1   last
+#
+# nums2: [2][5][6]
+#          ↑
+#          n-1
+#
+# ------------------------------------------------
+#
+# Step 2
+# Compare: 3 vs 5
+#
+# nums1: [1][2][3][0][5][6]
+#              ↑  ↑
+#              m-1 last
+#
+# nums2: [2][5][6]
+#      ↑
+#      n-1
+#
+# ------------------------------------------------
+#
+# Step 3
+# Compare: 3 vs 2
+#
+# nums1: [1][2][3][3][5][6]
+#          ↑ ↑
+#        m-1 last
+#
+# nums2: [2][5][6]
+#      ↑
+#      n-1
+#
+# ------------------------------------------------
+#
+# Step 4
+# Compare: 2 vs 2
+#
+# nums1: [1][2][2][3][5][6]
+#
+# nums2 finished ✅
+#
+# Memory:
+#
+# m-1 → Last valid element in nums1
+# n-1 → Last element in nums2
+# last → Last empty position in nums1
+#
+# Compare nums1[m-1] and nums2[n-1]
+# Bigger element → nums1[last]
+# Move that pointer
+# last--
+# Repeat
+#
+# Copy remaining nums2 elements (if any)
+#
+# Time : O(m + n)
+# Space: O(1)
